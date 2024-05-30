ea7c2afdebd0877089c411ec22d065bd06328707
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 16:36:34 +0100

    add VerticalListView and their class datasource

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,70 @@ main(){
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
+  
+
   @override
   Widget build(BuildContext context) {
+    List<MyItem> movies=[];
     return  MaterialApp(
       home: Scaffold(
-
+        body: VerticalListView( 
+          itemCount: movies.length + 1,
+          itemBuilder: (context, index) {
+            if (index < movies.length) {
+              return Container();
+            } else {
+              return  Container();
+            }
+          },
+        ),
       ),
     );
   }
+}
+
+class MyItem{
+  late String title;
+
+}
+
+
+class VerticalListView extends StatefulWidget {
+  final int itemCount;
+  final Widget Function(BuildContext context, int index) itemBuilder;
+  
+
+  const VerticalListView({
+    super.key,
+    required this.itemCount,
+    required this.itemBuilder,
+    
+  });
+
+  @override
+  State<VerticalListView> createState() => _VerticalListViewState();
+}
+
+class _VerticalListViewState extends State<VerticalListView> {
+  final _scrollController = ScrollController();
+
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      controller: _scrollController,
+      physics: const BouncingScrollPhysics(),
+      itemCount: widget.itemCount,
+      itemBuilder: widget.itemBuilder,
+      separatorBuilder: (context, index) {
+        return const SizedBox(height: 10);
+      },
+    );
+  }
+
+
 }
