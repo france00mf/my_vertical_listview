7fdca128d746db897e6e92160f996cea7b0939e5
France! <franciscodomingosjose06798@gmail.com>
2024-05-30 16:30:22 +0100

    first widget imp, mywidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 main(){
-  
+  runApp(MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+
+      ),
+    );
+  }
 }
