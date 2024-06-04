da42472861f7f506915e9cb1ea904a1398e167ff
France! <franciscodomingosjose06798@gmail.com>
2024-06-04 17:05:40 +0100

    fix json and add images

--- lib/main.dart
+++ lib/main.dart
@@ -219,3 +219,46 @@ class VerticalListViewCard extends StatelessWidget {
     );
   }
 }
+
+
+class ImageWithShimmer extends StatelessWidget {
+  const ImageWithShimmer({
+    super.key,
+    required this.imageUrl,
+    required this.width,
+    required this.height,
+  });
+
+  final String imageUrl;
+  final double height;
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    // return CachedNetworkImage(
+    //   imageUrl: imageUrl,
+    //   height: height,
+    //   width: width,
+    //   fit: BoxFit.cover,
+    //   placeholder: (_, __) => Shimmer.fromColors(
+    //     baseColor: Colors.grey[850]!,
+    //     highlightColor: Colors.grey[800]!,
+    //     child: Container(
+    //       height: height,
+    //       color: AppColors.secondaryText,
+    //     ),
+    //   ),
+    //   errorWidget: (_, __, ___) => const Icon(
+    //     Icons.error,
+    //     color: AppColors.error,
+    //   ),
+    // );
+
+     return  Image.asset(
+        "assets/img/"+imageUrl, 
+        height: height,
+        width: width,
+        fit: BoxFit.cover,
+      );
+  }
+}
